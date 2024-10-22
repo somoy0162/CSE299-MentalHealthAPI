fc0d480187d79a1b19d01188e36c131ab4ea9a7f
Sakibur Rahman Somoy <sakiburrahmansomoy4@gmail.com>
2024-10-22 14:47:11 +0600

    interfaces added

---
+++ MH.Services/Interfaces/IPermissionService.cs
@@ -0,0 +1,19 @@
+﻿using MH.Common.DTO;
+using MH.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MH.Services.Interfaces
+{
+    public interface IPermissionService
+    {
+        Task<ResponseMessage> GetAllPermission();
+        Task<ResponseMessage> GetAllPermissionActionByRoleID(int roleID);
+      
+        Task<ResponseMessage> SaveRolePermissionMapping(RolePermissionMapping rolePermissionMapping);
+
+    }
+}
--- MH.Services/Interfaces/ISystemUserService.cs
+++ MH.Services/Interfaces/ISystemUserService.cs
@@ -14,19 +14,9 @@ namespace MH.Services.Interfaces
         Task<ResponseMessage> GetAllSystemUser();
         Task<ResponseMessage> SaveSystemUser(SystemUsers user);
         Task<ResponseMessage> GetSystemUserById(int userID);
-        //Task<ResponseMessage> GetSystemUsersByRole(RequestMessage requestMessage);
-        //Task<ResponseMessage> GetSystemUserByUserName(RequestMessage requestMessage);
-        //Task<ResponseMessage> GetSystemUserByEmail(RequestMessage requestMessage);
         Task<ResponseMessage> DeleteSystemUserById(int userID);
-        //Task<ResponseMessage> GetDropdownOrganizationByUserID(RequestMessage requestMessage);
-        //Task<ResponseMessage> GetSystemUserDetails(RequestMessage requestMessage);
-        //Task<ResponseMessage> UpdateSystemUser(RequestMessage requestMessage);
-        Task<ResponseMessage> UpdatePassword(VMForgotPassword vmForgotPassword);
-        //Task<ResponseMessage> ChangePassword(RequestMessage requestMessage);
-        //Task<ResponseMessage> getSystemUsersForDropdown(RequestMessage requestMessage);
-        //Task<ResponseMessage> getAssigneeSystemUsersForDropdown(RequestMessage requestMessage);
-        //Task<ResponseMessage> GetSystemUsers(RequestMessage requestMessage);
-        //Task<ResponseMessage> ResetPassword(RequestMessage requestMessage);
-        //Task<ResponseMessage> SendMailForgotPassword(string userName);
+        Task<ResponseMessage> UpdatePersonalDetails(VMLogin users);
+        Task<ResponseMessage> UpdatePassword(VMPasswordChange param);
+        Task<ResponseMessage> GetAllGender();
     }
 }
